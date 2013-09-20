5ee394af69bd16bf5a7c088cfeacc223fd335158
SuperSpyTX (Joe) <r29jk10@gmail.com>
2013-09-19 22:48:35 -0500

    Revert "Consolfagits should die in a fire.  Because consoles are overpriced cardboard boxes that don't mean jack shit and are proprietary."
    This reverts commit 3291454200240ea68bb358db4966c795f3ca0b1c.

--- app/routing/default.php
+++ app/routing/default.php
@@ -11,9 +11,7 @@ if (!defined("SF_INIT")) {
 
 class DefaultRoute implements Supah_Framework\IRoute {
 	public function route($uri) {
-		$cocks = "";
-		for ($i = 0; $i < 100; $i++) $cocks .= "COCKS TITS ASS BITCH<br>";
-		echo("Grand Theft Auto 5 Consolfagits because piracy?... wow. <br>".$cocks."<br><br> Meanwhile in iTunes... <br><br>\"People like us we've gotta stick together keep your head up, nothing lasts forever Here's to the damned, to the lost and forgotten.  It's hard to get high when you're living on the bottom\" - Kelly Clarkson");
+		echo("You wat.<br><br> Meanwhile in iTunes... <br><br>\"People like us we've gotta stick together keep your head up, nothing lasts forever Here's to the damned, to the lost and forgotten.  It's hard to get high when you're living on the bottom\" - Kelly Clarkson");
 	}
 
 	public function ruleMatches($uri) {
